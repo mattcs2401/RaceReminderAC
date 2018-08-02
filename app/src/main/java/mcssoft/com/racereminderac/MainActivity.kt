f10a573fa17745bea844f140a4d7dfd21366f290
mattcs2401 <mattcs2401@gmail.com>
2018-08-02 13:22:55 +1000

    Code change and more initial commit.

--- app/src/main/java/mcssoft/com/racereminderac/MainActivity.kt
+++ app/src/main/java/mcssoft/com/racereminderac/MainActivity.kt
@@ -2,6 +2,9 @@ package mcssoft.com.racereminderac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import mcssoft.com.racereminderac.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+            addFragment(R.id.container, MainFragment.newInstance())
+//            supportFragmentManager.beginTransaction()
+//                    .replace(R.id.container, MainFragment.newInstance())
+//                    .commitNow()
         }
     }
 
+    // Kotlin trickery.
+    // https://medium.com/thoughts-overflow/how-to-add-a-fragment-in-kotlin-way-73203c5a450b
+    private fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+
+    fun AppCompatActivity.addFragment(frameId: Int, fragment: Fragment){
+        supportFragmentManager.inTransaction { add(frameId, fragment) }
+    }
+
+
+    fun AppCompatActivity.replaceFragment(frameId: Int, fragment: Fragment) {
+        supportFragmentManager.inTransaction{replace(frameId, fragment)}
+    }
 }
