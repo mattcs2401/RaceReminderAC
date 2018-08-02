ae8f5f415bde2d867487e3f7b600699a70b80b7e
mattcs2401 <mattcs2401@gmail.com>
2018-08-02 13:07:12 +1000

    Initial commit

---
+++ app/src/main/java/mcssoft/com/racereminderac/MainActivity.kt
@@ -0,0 +1,19 @@
+package mcssoft.com.racereminderac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mcssoft.com.racereminderac.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/mcssoft/com/racereminderac/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package mcssoft.com.racereminderac.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
