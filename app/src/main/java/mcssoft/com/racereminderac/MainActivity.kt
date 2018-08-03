d2e7266ce4726e4a7db3992dfd5b35d2cc2a36b9
mattcs2401 <mattcs2401@gmail.com>
2018-08-03 13:52:33 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/MainActivity.kt
+++ app/src/main/java/mcssoft/com/racereminderac/MainActivity.kt
@@ -1,27 +1,15 @@
 package mcssoft.com.racereminderac
 
-import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import mcssoft.com.racereminderac.ui.main.EditFragment
-import mcssoft.com.racereminderac.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() { //}, View.OnClickListener {
 // https://willowtreeapps.com/ideas/exploring-androids-navigation-architecture-component
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
@@ -29,9 +17,7 @@ class MainActivity : AppCompatActivity() { //}, View.OnClickListener {
         val host: NavHostFragment = supportFragmentManager
                 .findFragmentById(R.id.id_nav_host_fragment) as NavHostFragment? ?: return
 
-        val navController = host.navController
-
-        setupBottomNavMenu(navController)
+        setupBottomNavMenu(host.navController)
 
     }
 
