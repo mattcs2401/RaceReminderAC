06d7e400225e967ecc04bf842ab8604192935769
mattcs2401 <03m777c22s>
2018-08-06 21:10:58 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/MainActivity.kt
+++ app/src/main/java/mcssoft/com/racereminderac/MainActivity.kt
@@ -2,34 +2,30 @@ package mcssoft.com.racereminderac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() { //}, View.OnClickListener {
-// https://willowtreeapps.com/ideas/exploring-androids-navigation-architecture-component
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        val host: NavHostFragment = supportFragmentManager
-                .findFragmentById(R.id.id_nav_host_fragment) as NavHostFragment? ?: return
+        val navController = findNavController(R.id.id_nav_host_fragment)
+        val fabAdd: FloatingActionButton = findViewById<FloatingActionButton>(R.id.id_fab)
+        Navigation.setViewNavController(fabAdd, navController)
 
-        setupBottomNavMenu(host.navController)
-
-    }
-
-//    override fun onClick(view: View) {
-//        when (view.id) {
-//        }
-//    }
-
-    private fun setupBottomNavMenu(navController: NavController) {
-        findViewById<BottomNavigationView>(R.id.bottom_nav_view)?.let { bottomNavView ->
-            NavigationUI.setupWithNavController(bottomNavView, navController)
+        fabAdd.setOnClickListener {
+            navController.navigate(R.id.id_edit_fragment)
         }
     }
 
 }
+/*
+val navController = findNavController(R.id.navHostFragment)
+Navigation.setViewNavController(fabAdd, navController)
+fabAdd.setOnClickListener {
+navController.navigate(R.id.addSymbolFragment)
+}
+*/
