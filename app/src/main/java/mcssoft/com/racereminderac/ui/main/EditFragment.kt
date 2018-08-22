378c1ef8c4682774e685710baafcd2ecef97d259
mattcs2401 <mattcs2401@gmail.com>
2018-08-22 13:41:43 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/ui/main/EditFragment.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/main/EditFragment.kt
@@ -11,6 +11,10 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.Navigation
+import butterknife.BindString
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.Unbinder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import mcssoft.com.racereminderac.R
 import mcssoft.com.racereminderac.entity.Race
@@ -25,6 +29,7 @@ class EditFragment : Fragment(), View.OnClickListener, View.OnTouchListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         rootView = inflater.inflate(R.layout.edit_fragment, container, false)
+        //unbinder = ButterKnife.bind(rootView)
         return rootView
     }
 
@@ -39,17 +44,22 @@ class EditFragment : Fragment(), View.OnClickListener, View.OnTouchListener {
             when(editType) {
                 "edit_type_existing" -> {
                     toolBar.title = "Edit Race"
-                    btnSave.setText("Update")
+                    btnSave.text = "Update"
                     val race = arguments?.getParcelable<Race>(getString(R.string.key_edit_existing))
                     populateFromArgs(race)
                 }
                 "edit_type_new" -> {
                     toolBar.title = "New Race"
-                    btnSave.setText("Save")
+                    btnSave.text = "Save"
                 }
             }
         }
     }
+
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        unbinder.unbind()
+//    }
 
     override fun onClick(view: View) {
         when(view.id) {
@@ -143,4 +153,12 @@ class EditFragment : Fragment(), View.OnClickListener, View.OnTouchListener {
 
     private var editType: String? = null
 
+    // this doesn't work.
+    //private lateinit var unbinder: Unbinder
+    //@BindString(R.string.save) lateinit var save: String
+    //@BindString(R.string.update) lateinit var update: String
+    //@BindString(R.string.edit_race) lateinit var edit_race: String
+    //@BindString(R.string.new_race) lateinit var new_race: String
+    //@BindString(R.string.edit_type_existing) lateinit var edit_type_existing: String
+    //@BindString(R.string.edit_type_new) lateinit var edit_type_new: String
 }
