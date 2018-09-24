86366e8f68a6d2935a761efcb58fbf683ad7a208
mattcs2401 <mattcs2401@gmail.com>
2018-09-24 13:02:10 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/ui/dialog/RaceCodesDialog.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/dialog/RaceCodesDialog.kt
@@ -1,39 +1,33 @@
 package mcssoft.com.racereminderac.ui.dialog
 
-import android.app.AlertDialog
-import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
-import android.content.DialogInterface
-import android.preference.DialogPreference
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import android.widget.Button
 import mcssoft.com.racereminderac.R
-import mcssoft.com.racereminderac.interfaces.ICodes
 import mcssoft.com.racereminderac.utility.MessageEvent
 import org.greenrobot.eventbus.EventBus
-import android.view.Window.FEATURE_NO_TITLE
-
-
 
 
 class RaceCodesDialog : DialogFragment(), View.OnClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.race_codes2, container, false)
+        return inflater.inflate(R.layout.race_codes, container, false)
+//        return inflater.inflate(R.layout.race_codes2, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // set the listeners.
         (view.findViewById<Button>(R.id.id_rc_btn_R)).setOnClickListener(this)
         (view.findViewById<Button>(R.id.id_rc_btn_T)).setOnClickListener(this)
         (view.findViewById<Button>(R.id.id_rc_btn_G)).setOnClickListener(this)
         (view.findViewById<Button>(R.id.id_rc_btn_S)).setOnClickListener(this)
         (view.findViewById<Button>(R.id.id_rc_btn_cancel)).setOnClickListener(this)
+        // special case or the OK button.
         btnOK = view.findViewById<Button>(R.id.id_rc_btn_ok)
         btnOK.setOnClickListener(this)
         btnOK.isEnabled = false
@@ -55,8 +49,6 @@ class RaceCodesDialog : DialogFragment(), View.OnClickListener {
         }
     }
 
-    private val OK: Int = -1
-    private val CANCEL: Int = -2
-    private lateinit var btnOK: Button
     private var raceCode: String = ""
+    private lateinit var btnOK: Button
 }
