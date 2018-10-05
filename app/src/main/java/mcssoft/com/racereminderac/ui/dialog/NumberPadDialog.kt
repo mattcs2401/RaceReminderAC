88f17aa7aeaea5d8ec1d44557893c535f38daa76
mattcs2401 <mattcs2401@gmail.com>
2018-10-05 14:36:42 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/ui/dialog/NumberPadDialog.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/dialog/NumberPadDialog.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.number_pad.*
 import mcssoft.com.racereminderac.R
 import mcssoft.com.racereminderac.utility.EventMessage
 import org.greenrobot.eventbus.EventBus
@@ -25,6 +24,8 @@ class NumberPadDialog : DialogFragment(), DialogInterface.OnClickListener, View.
         rootView = inflater.inflate(R.layout.number_pad, null)
         initialiseViews(rootView)
 
+        args = arguments!!.getInt("key")
+
         // build the dialog.
         val builder: AlertDialog.Builder = AlertDialog.Builder(this.context!!)
         builder.setTitle("Enter a number")
@@ -35,18 +36,26 @@ class NumberPadDialog : DialogFragment(), DialogInterface.OnClickListener, View.
     }
 
     override fun onClick(view: View) {
-        number = (view as Button).text.toString()
+        // TODO - implement on the fly checking digit validation.
+        if(number.isBlank()) {
+            number = (view as Button).text.toString()
+        } else {
+            // TBA - validation
+            if(number.length < 3) {
+                number += (view as Button).text.toString()
+            }
+        }
     }
 
     override fun onClick(dialog: DialogInterface?, which: Int) {
         var evntMsg: EventMessage? = null
         when(which) {
             Dialog.BUTTON_POSITIVE -> {
-                if(number == null) {
+                if(number.isBlank()) {
                     evntMsg = EventMessage("", R.integer.number_pad_dialog_id, R.integer.ctxNoNumber)
                 } else {
                     arguments?.getInt("key")
-                    evntMsg = EventMessage(number!!, R.integer.number_pad_dialog_id, arguments!!.getInt("key"))
+                    evntMsg = EventMessage(number, R.integer.number_pad_dialog_id, args)
                 }
                 EventBus.getDefault().post(evntMsg)
             }
@@ -54,20 +63,30 @@ class NumberPadDialog : DialogFragment(), DialogInterface.OnClickListener, View.
         this.dialog.dismiss()
     }
 
-    private fun initialiseViews(view: View) {
-        // Note: 'synthetic' didn't seem to work here ?
-        (view.findViewById<Button>(R.id.id_btn_0)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_1)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_2)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_3)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_4)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_5)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_6)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_7)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_8)).setOnClickListener(this)
-        (view.findViewById<Button>(R.id.id_btn_9)).setOnClickListener(this)
+    private fun validate() {
+        // TBA
     }
 
+    private fun initialiseViews(view: View) {
+        // Note: 'synthetic' didn't seem to work here ?
+        btn0 = view.findViewById<Button>(R.id.id_np_btn_0)
+        btn0.setOnClickListener(this)
+        btn0.isEnabled = false
+        //(view.findViewById<Button>(R.id.id_np_btn_0)).setOnClickListener(this)
+
+        (view.findViewById<Button>(R.id.id_np_btn_1)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_2)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_3)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_4)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_5)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_6)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_7)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_8)).setOnClickListener(this)
+        (view.findViewById<Button>(R.id.id_np_btn_9)).setOnClickListener(this)
+    }
+
+    private lateinit var btn0: Button
     private lateinit var rootView: View     // dialog's main view.
-    private var number: String? = null      // selected button value.
+    private var number: String = ""         // selected button value.
+    private var args: Int = 0
 }
