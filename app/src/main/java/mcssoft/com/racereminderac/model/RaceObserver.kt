4ded04761b1546438d32fa872964278c31acc083
mattcs2401 <mattcs2401@gmail.com>
2018-10-16 08:49:13 +1000

    Code updates.

--- app/src/main/java/mcssoft/com/racereminderac/model/RaceObserver.kt
+++ app/src/main/java/mcssoft/com/racereminderac/model/RaceObserver.kt
@@ -3,7 +3,7 @@ package mcssoft.com.racereminderac.model
 import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.edit_fragment.view.*
+import kotlinx.android.synthetic.main.edit_fragment2.view.*
 import mcssoft.com.racereminderac.entity.Race
 
 class RaceObserver(race: LiveData<Race>, view: View) : Observer<Race> {
@@ -17,11 +17,11 @@ class RaceObserver(race: LiveData<Race>, view: View) : Observer<Race> {
     override fun onChanged(race: Race?) {
         if(race != null) {
             /* surround with null check so will skip whole block if is null. */
-            rootView.id_etCityCode.setText(race.cityCode)
-            rootView.id_etRaceCode.setText(race.raceCode)
-            rootView.id_etRaceNum.setText(race.raceNum)
-            rootView.id_etRaceSel.setText(race.raceSel)
-            rootView.id_etRaceTime.setText(race.raceTime)
+//            rootView.id_etCityCode.setText(race.cityCode)
+//            rootView.id_etRaceCode.setText(race.raceCode)
+//            rootView.id_etRaceNum.setText(race.raceNum)
+//            rootView.id_etRaceSel.setText(race.raceSel)
+//            rootView.id_etRaceTime.setText(race.raceTime)
         }
     }
 
--- app/src/main/java/mcssoft/com/racereminderac/ui/fragment/EditFragment2.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/fragment/EditFragment2.kt
@@ -7,26 +7,19 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.NumberPicker
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.android.synthetic.main.edit_fragment.*
 import kotlinx.android.synthetic.main.edit_fragment2.*
 import kotlinx.android.synthetic.main.toolbar_base.*
 import mcssoft.com.racereminderac.entity.Race
-import mcssoft.com.racereminderac.model.RaceObserver
 import mcssoft.com.racereminderac.model.RaceViewModel
-import org.greenrobot.eventbus.EventBus
 import mcssoft.com.racereminderac.R
-import mcssoft.com.racereminderac.utility.EventMessage
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
+import mcssoft.com.racereminderac.model.RaceObserver
 
 class EditFragment2 : Fragment(), View.OnClickListener , View.OnTouchListener {
 
@@ -54,46 +47,22 @@ class EditFragment2 : Fragment(), View.OnClickListener , View.OnTouchListener {
 //    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
 //        super.onCreateContextMenu(menu, v, menuInfo)
 //    }
-
-//    override fun onStart() {
-//        super.onStart()
-//        EventBus.getDefault().register(this)
-//    }
-
-//    override fun onStop() {
-//        super.onStop()
-//        EventBus.getDefault().unregister(this)
-//    }
     //</editor-fold>
-
-//    /**
-//     * EventBus returns here.
-//     * @param event - The EventBus message object.
-//     */
-//    @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
-//    fun onMessageEvent(event: EventMessage) {
-//        if(!event.message.isBlank()) {
-////            doOnMessageEvent(event)
-//        } else {
-//            // Nothing was selected in the dialog except for the OK button.
-////            doSnackbar(event.ident, event.ctx)
-//        }
-//    }
 
     //<editor-fold defaultstate="collapsed" desc="Region: Event handler - onClick">
     override fun onClick(view: View) {
         when(view.id) {
             R.id.id_btn_save -> {
                 if(checkValues()) {
-//                    var race = collateValues()
-//                    when(editType) {
-//                        R.integer.edit_race_existing -> {
-//                            raceViewModel.update(race)
-//                        }
-//                        R.integer.edit_race_new -> {
-//                            raceViewModel.insert(race)
-//                        }
-//                    }
+                    var race = collateValues()
+                    when(editType) {
+                        R.integer.edit_race_existing -> {
+                            raceViewModel.update(race)
+                        }
+                        R.integer.edit_race_new -> {
+                            raceViewModel.insert(race)
+                        }
+                    }
                     Navigation.findNavController(activity!!, R.id.id_nav_host_fragment)
                             .navigate(R.id.id_main_fragment)
                 }
@@ -130,11 +99,11 @@ class EditFragment2 : Fragment(), View.OnClickListener , View.OnTouchListener {
         when(editType) {
             resources.getInteger(R.integer.edit_race_existing) -> {
                 toolBar.title = getString(R.string.edit_race)
-//                btnSave.text = getString(R.string.update)
+                btnSave.text = getString(R.string.update)
             }
             resources.getInteger(R.integer.edit_race_new) -> {
                 toolBar.title = getString(R.string.new_race)
-//                btnSave.text = getString(R.string.save)
+                btnSave.text = getString(R.string.save)
             }
         }
     }
@@ -151,13 +120,8 @@ class EditFragment2 : Fragment(), View.OnClickListener , View.OnTouchListener {
      * get the UI values into a Race object ready for Update or Insert.
      */
     private fun collateValues(): Race {
-        val race = Race("","","","",""
-//                etCityCode.text.toString(),
-//                etRaceCode.text.toString(),
-//                etRaceNum.text.toString(),
-//                etRaceSel.text.toString(),
-//                etRaceTime.text.toString()
-        )
+        // TODO - get NumberPicker values.
+        val race = Race("","","","","")
         race.id = raceId
         return race
     }
@@ -167,65 +131,57 @@ class EditFragment2 : Fragment(), View.OnClickListener , View.OnTouchListener {
         (activity?.findViewById(R.id.id_fab) as FloatingActionButton).hide()
 
         // Set the Save button and listener.
-//        btnSave = id_btn_save
-//        btnSave.setOnClickListener(this)
+        btnSave = id_btn_save
+        btnSave.setOnClickListener(this)
 
         // Get the toolbar.
         toolBar = activity?.id_toolbar as Toolbar
 
+        // Set the number pickers.
         npCityCode = id_np_city_code
-        
+        val ccVals = resources.getStringArray(R.array.cityCodes)
+        npCityCode.minValue = 0
+        npCityCode.maxValue = ccVals.size - 1
+        npCityCode.displayedValues = ccVals
+        npCityCode.wrapSelectorWheel = true
 
-//        // Get the Race related views.
-//        etCityCode = id_etCityCode
-//        etCityCode.setOnTouchListener(this)
-//        etCityCode.showSoftInputOnFocus = false  // use dialog.
-//
-//        etRaceCode = id_etRaceCode
-//        etRaceCode.setOnTouchListener(this)
-//        etRaceCode.showSoftInputOnFocus = false  // use dialog.
-//
-//        etRaceNum = id_etRaceNum
-//        etRaceNum.setOnTouchListener(this)
-//        etRaceNum.showSoftInputOnFocus = false  // use dialog.
-//
-//        etRaceSel = id_etRaceSel
-//        etRaceSel.setOnTouchListener(this)
-//        etRaceSel.showSoftInputOnFocus = false  // use dialog.
-//
-//        etRaceTime = id_etRaceTime
-//        etRaceTime.setOnTouchListener(this)
-//        etRaceTime.showSoftInputOnFocus = false
+        npRaceCode = id_np_race_code
+        val rcVals = resources.getStringArray(R.array.raceCodes)
+        npRaceCode.minValue = 0
+        npRaceCode.maxValue = rcVals.size - 1
+        npRaceCode.displayedValues = rcVals
+        npRaceCode.wrapSelectorWheel = true
+
+        npRaceNo = id_np_race_num
+        val rnVals = resources.getStringArray(R.array.raceNum)
+        npRaceNo.minValue = 0
+        npRaceNo.maxValue = rnVals.size - 1
+        npRaceNo.displayedValues = rnVals
+        npRaceNo.wrapSelectorWheel = true
+
+        npRaceSel = id_np_race_sel
+        val rsVals = resources.getStringArray(R.array.raceSel)
+        npRaceSel.minValue = 0
+        npRaceSel.maxValue = rsVals.size - 1
+        npRaceSel.displayedValues = rsVals
+        npRaceSel.wrapSelectorWheel = true
 
         // view model.
-//        raceViewModel = ViewModelProviders.of(activity!!).get(RaceViewModel::class.java)
-//        raceId = arguments?.getLong(getString(R.string.key_edit_existing))
-//        raceViewModel.getRace(raceId!!).observe(activity!!, RaceObserver(raceViewModel.getRace(raceId!!), view))
-
+        raceViewModel = ViewModelProviders.of(activity!!).get(RaceViewModel::class.java)
+        raceId = arguments?.getLong(getString(R.string.key_edit_existing))
+        raceViewModel.getRace(raceId!!).observe(activity!!, RaceObserver(raceViewModel.getRace(raceId!!), view))
     }
-
 
     //<editor-fold defaultstate="collapsed" desc="Region: Private Vars">
     private lateinit var toolBar: Toolbar
     private lateinit var npCityCode: NumberPicker
-
+    private lateinit var npRaceCode: NumberPicker
+    private lateinit var npRaceNo: NumberPicker
+    private lateinit var npRaceSel: NumberPicker
     private var raceId: Long? = null
-//    private lateinit var etCityCode : EditText
-//    private lateinit var etRaceCode : EditText
-//    private lateinit var etRaceNum : EditText
-//    private lateinit var etRaceSel : EditText
-//    private lateinit var etRaceTime : EditText
-    private lateinit var btnSave: Button
-
-    private lateinit var raceViewModel: RaceViewModel
-
     private var editType: Int? = null
-
-//    private lateinit var cityCodesDialog: DialogFragment
-//    private lateinit var raceCodesDialog: DialogFragment
-//    private lateinit var numberPadDialog: DialogFragment
-//    private lateinit var timePickDialog: DialogFragment
-
+    private lateinit var btnSave: Button
+    private lateinit var raceViewModel: RaceViewModel
     private lateinit var rootView: View
     //</editor-fold>
 }
