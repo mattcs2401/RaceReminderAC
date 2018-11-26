45f8081eaca96fb2e31be6ae4367ccc5e29e7fa5
mattcs2401 <mattcs2401@gmail.com>
2018-11-26 14:34:37 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/entity/Race.kt
+++ app/src/main/java/mcssoft/com/racereminderac/entity/Race.kt
@@ -9,11 +9,19 @@ data class Race(@ColumnInfo(name = "CityCode") var cityCode: String,
                 @ColumnInfo(name = "RaceCode") var raceCode: String,
                 @ColumnInfo(name = "RaceNum")  var raceNum: String,
                 @ColumnInfo(name = "RaceSel")  var raceSel: String,
-                @ColumnInfo(name = "RaceTime") var raceTime: String) {
+                @ColumnInfo(name = "RaceTime") var raceTime: String) : Comparable<Race> {
 
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id") var id: Long? = null
 
     @ColumnInfo(name = "RaceDate") var raceDate: String = "01/01/2019"
     @ColumnInfo(name = "ArchvRace") var archvRace: String = "N"
+
+    /**
+     * Simple compare on RaceDate.
+     * @param other: The object to compare (this) against.
+     */
+    override fun compareTo(other: Race): Int {
+        return raceDate.compareTo(other.raceDate)
+    }
 }
