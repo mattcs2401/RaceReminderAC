7d035eb3fc5bbae7b97e338a1ec07f2b2513a0b8
mattcs2401 <mattcs2401@gmail.com>
2018-12-13 08:34:49 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/background/worker/NotifyWorker.kt
+++ app/src/main/java/mcssoft/com/racereminderac/background/worker/NotifyWorker.kt
@@ -1,7 +1,9 @@
 package mcssoft.com.racereminderac.background.worker
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
@@ -15,70 +17,108 @@ import mcssoft.com.racereminderac.R
 import mcssoft.com.racereminderac.utility.Constants
 
 class NotifyWorker(private val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
-// TODO - "fancy" up the notification.
+// TODO - "fancy" up the notification ?
 
     override fun doWork(): Result {
-        val map = inputData.keyValueMap
-        val id = map["key_id"] as Long       // object id passed in the arguments to the fragment.
-        val cc = map["key_cc"] as String    // city code, notification display purposes.
-        val rc  = map["key_rc"] as String   // race code, notification display purposes.
-        val rn  = map["key_rn"] as String   // race num, notification display purposes.
-        val rs  = map["key_rs"] as String   // race set, notification display purposes.
-        val rt  = map["key_rt"] as String   // race time, notification display purposes.
+        val message = createMessage()
+        val id = inputData.keyValueMap["key_id"] as Long
+        val title = context.resources.getString(R.string.nearing_race_time)
 
         try {
-            sendNotification("Nearing Race time.", "$cc$rc $rn $rs $rt", id)
-//            return Result.success()
+            displayNotification(title, message, id)
+            return Result.success()
         } catch (ex: Exception) {
             return Result.failure()
         } finally {
             // TBA
         }
-        return Result.success()
     }
 
-    private fun sendNotification(title: String, message: String, id: Long) {
+    /**
+     * Simple utility function to create the notification message.
+     * @return A string from the input data values.
+     */
+    private fun createMessage() : String {
+        val map = inputData.keyValueMap
+        val cc = map["key_cc"] as String    // city code, notification display purposes.
+        val rc  = map["key_rc"] as String   // race code, notification display purposes.
+        val rn  = map["key_rn"] as String   // race num, notification display purposes.
+        val rs  = map["key_rs"] as String   // race set, notification display purposes.
+        val rt  = map["key_rt"] as String   // race time, notification display purposes.
+        return "$cc$rc $rn $rs $rt"
+    }
+
+    /**
+     * Simple utility function to display the Notification.
+     * @param title: The notification's title.
+     * @param message: The notification's message.
+     * @param id: Object's id passed in the arguments to the notification's PendingIntent.
+     */
+    private fun displayNotification(title: String, message: String, id: Long) {
         /**
-         * Notes (11/12/2018):
          * "Compat" methods used primarily as this project has min SDK version of 22.
-         * The key is the NavDeepLinkBuilder method to create the pending intent.
+         * The key is the NavDeepLinkBuilder method to create the PendingIntent.
          * See: https://proandroiddev.com/android-jetpack-navigation-to-the-rescue-fe588271d36
          *      https://developer.android.com/training/notify-user/build-notification
          */
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-//        //If on Oreo then notification requires a notification channel.
+        //If on Oreo then Notification requires a notification channel.
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             val channel = NotificationChannel("default", "Default", NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
         }
 
-        // Prepare the arguments to pass in the notification.
-        val arguments = Bundle().apply {
-            putLong(context.getString(R.string.key_edit_existing), id)
-            putInt(context.getString(R.string.key_edit_type), Constants.EDIT_RACE_UPDATE)
-        }
+        // Create the Notification.
+        val notification = createNotification(title, message, id)
 
-        // Prepare the pending intent, while specifying the graph and destination.
-        val pendingIndent = NavDeepLinkBuilder(context)
-                .setGraph(R.navigation.navigation)
-                .setDestination(R.id.id_edit_fragment)
-                .setArguments(arguments)
-                .createPendingIntent()
+        // Display the notification.
+        NotificationManagerCompat.from(context).notify(10, notification)
+    }
 
-        // Create the notification instance.
-        val notification = NotificationCompat.Builder(context, "default")
+    /**
+     * Simple utility function to create the Notification.
+     * @param title: The notification's title.
+     * @param message: The notification's message.
+     * @param id: Object's id passed in the arguments to the PendingIntent.
+     * @return A Notification.
+     */
+    private fun createNotification(title: String, message: String, id: Long) : Notification {
+        return NotificationCompat.Builder(context, "default")
 //                .setStyle(NotificationCompat.DecoratedCustomViewStyle())
                 .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setContentIntent(pendingIndent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.DEFAULT_VIBRATE.toString())
+                .setContentIntent(createIntent(id))
                 .setAutoCancel(true)
                 .build()
+    }
 
-        // Display the notification.
-//        NotificationManagerCompat.from(context).notify(10, notification)
-        notificationManager.notify(1, notification)//.build())
+    /**
+     * Simple utility function to create a PendingIntent for the notification.
+     * @param id: The object's id passed in the arguments.
+     * @return A PendingIntent.
+     */
+    private fun createIntent(id: Long) : PendingIntent {
+        return NavDeepLinkBuilder(context)
+                .setGraph(R.navigation.navigation)
+                .setDestination(R.id.id_edit_fragment)
+                .setArguments(createArguments(id))
+                .createPendingIntent()
+    }
+
+    /**
+     * Simple utility function to create the arguments to be passed to the PendingIntent..
+     * @param id: The object's id passed in the bundle.
+     * @return A Bundle.
+     */
+    private fun createArguments(id: Long) : Bundle {
+        return Bundle().apply {
+            putLong(context.getString(R.string.key_edit_existing), id)
+            putInt(context.getString(R.string.key_edit_type), Constants.EDIT_RACE_UPDATE)
+        }
     }
 }
--- app/src/main/java/mcssoft/com/racereminderac/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/fragment/SettingsFragment.kt
@@ -1,9 +1,9 @@
 package mcssoft.com.racereminderac.ui.fragment
 
 import android.os.Bundle
-import android.preference.Preference
 import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.toolbar_base.*
@@ -18,9 +18,17 @@ class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceClic
         (activity?.findViewById(R.id.id_bottom_nav_view) as BottomNavigationView).visibility = View.GONE
         // Load the preferences from the XML resource.
         addPreferencesFromResource(R.xml.preferences)
+
+        findPreference("key_maint_del_archived").onPreferenceClickListener = this
+
     }
 
     override fun onPreferenceClick(preference: Preference?): Boolean {
+        when(preference!!.key) {
+            "key_maint_del_archived" -> {
+                val bp = ""
+            }
+        }
 
         return true
     }
