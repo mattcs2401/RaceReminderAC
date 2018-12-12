bfceb3b8fa36bcdc2491a0db17a8709f4089ccc8
mattcs2401 <mattcs2401@gmail.com>
2018-12-12 12:29:13 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/ui/fragment/Settings.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/fragment/Settings.kt
@@ -1,10 +1,11 @@
 package mcssoft.com.racereminderac.ui.fragment
 
 import android.os.Bundle
+import android.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import mcssoft.com.racereminderac.R
 
-class Settings : PreferenceFragmentCompat() {
+class Settings : PreferenceFragmentCompat(), Preference.OnPreferenceClickListener, Preference.OnPreferenceChangeListener {
 
     companion object {
         fun getInstance(): Settings = Settings()
@@ -15,4 +16,13 @@ class Settings : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.settings, rootKey)
     }
 
+    override fun onPreferenceClick(preference: Preference?): Boolean {
+
+        return true
+    }
+
+    override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+
+        return true
+    }
 }
