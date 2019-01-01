88e75abb3417b93e8d07e424377efe17c5a39440
mattcs2401 <03m777c22s>
2019-01-02 07:46:20 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/ui/fragment/MainFragment.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/fragment/MainFragment.kt
@@ -89,7 +89,7 @@ class MainFragment : Fragment(), ISelect.ItemSelect, ISelect.ItemLongSelect {
     @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
     fun onMessageEvent(refresh: RefreshMessage) {
         // Note: the down side to this is the observer will react twice.
-        if(raceAdapter != null && raceAdapter.itemCount > 0) {
+        if(raceAdapter.itemCount > 0) {
            // Get arbitary 1st Race from backing data.
             val race = raceAdapter.getRace(0)
             val oldCc = race.cityCode
--- app/src/main/java/mcssoft/com/racereminderac/utility/eventbus/RefreshMessage.kt
+++ app/src/main/java/mcssoft/com/racereminderac/utility/eventbus/RefreshMessage.kt
@@ -1,8 +1,3 @@
 package mcssoft.com.racereminderac.utility.eventbus
 
-import mcssoft.com.racereminderac.entity.Race
-
-class RefreshMessage() { //var race: Race) {
-
-//    val theRace: Race get() = race
-}
+class RefreshMessage { }
