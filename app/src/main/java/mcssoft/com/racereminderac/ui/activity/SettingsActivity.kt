8399305d9fa5d19ab0b272075cd2e8e1fcc58cc8
mattcs2401 <mattcs2401@gmail.com>
2019-01-16 14:55:56 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/ui/activity/SettingsActivity.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/activity/SettingsActivity.kt
@@ -2,10 +2,10 @@ package mcssoft.com.racereminderac.ui.activity
 
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import androidx.appcompat.app.AppCompatActivity
 import mcssoft.com.racereminderac.R
-import mcssoft.com.racereminderac.R.xml.preference_headers
 
-class SettingsActivity : PreferenceActivity() {
+class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,12 +13,16 @@ class SettingsActivity : PreferenceActivity() {
 
     }
 
-    /**
-     * Populate the activity with the top-level headers.
-     */
-    override fun onBuildHeaders(target: List<PreferenceActivity.Header>) {
-        loadHeadersFromResource(R.xml.preference_headers, target)
-    }
+//    /**
+//     * Populate the activity with the top-level headers.
+//     */
+//    override fun onBuildHeaders(target: List<PreferenceActivity.Header>) {
+//        loadHeadersFromResource(R.xml.preference_headers, target)
+//    }
+
+//    override fun isValidFragment(fragmentName: String?): Boolean {
+//        return true //super.isValidFragment(fragmentName)
+//    }
+    // mcssoft.com.racereminderac.ui.fragment.settings.DefaultsFragment
+
 }
-
-
--- app/src/main/java/mcssoft/com/racereminderac/ui/fragment/settings/HeaderFragment.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/fragment/settings/HeaderFragment.kt
@@ -1,14 +1,25 @@
 package mcssoft.com.racereminderac.ui.fragment.settings
 
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import mcssoft.com.racereminderac.R
 
-class HeaderFragment : PreferenceFragmentCompat() {
+/**
+ * Utility type class as starting point for the preferences navigation. Just displays a "switchboard".
+ */
+class HeaderFragment : Fragment() {
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        // Load the preferences from the XML resource.
-        addPreferencesFromResource(R.xml.preference_headers)
-
+    //<editor-fold defaultstate="collapsed" desc="Region: Lifecycle">
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.header_fragment, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    //</editor-fold>
 }
