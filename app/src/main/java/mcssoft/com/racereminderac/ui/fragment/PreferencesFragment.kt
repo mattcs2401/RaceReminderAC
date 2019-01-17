463524bcfe51031022c0563682fbe79b20b3e6b3
mattcs2401 <mattcs2401@gmail.com>
2019-01-17 13:45:22 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/ui/fragment/PreferencesFragment.kt
+++ app/src/main/java/mcssoft/com/racereminderac/ui/fragment/PreferencesFragment.kt
@@ -2,12 +2,13 @@ package mcssoft.com.racereminderac.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.toolbar_base.*
 import mcssoft.com.racereminderac.R
 
-class PreferencesFragment : PreferenceFragmentCompat() {
+class PreferencesFragment : PreferenceFragmentCompat(),Preference.OnPreferenceClickListener, Preference.OnPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
@@ -19,4 +20,16 @@ class PreferencesFragment : PreferenceFragmentCompat() {
         activity?.findViewById<BottomNavigationView>(R.id.id_bottom_nav_view)?.visibility = View.GONE
         (activity?.id_toolbar)?.title = getString(R.string.preferences)
     }
+
+    override fun onPreferenceClick(preference: Preference?): Boolean {
+        // TODO - onPreferenceClick
+        return true
+    }
+
+    override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+        // TODO - onPreferenceChange
+        return true
+    }
+
+
 }
