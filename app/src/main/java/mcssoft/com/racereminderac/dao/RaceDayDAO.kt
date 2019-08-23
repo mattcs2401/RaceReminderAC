4342a924a7765b28d5436a6e5281bceb4101f2c9
mattcs2401 <mattcs2401@gmail.com>
2019-08-23 14:51:06 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/dao/RaceDayDAO.kt
+++ app/src/main/java/mcssoft/com/racereminderac/dao/RaceDayDAO.kt
@@ -10,35 +10,45 @@ import mcssoft.com.racereminderac.entity.xml.Runner
 interface RaceDayDAO {
 
     // Inserts.
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    fun insertRaceDay(raceDay: RaceDay): Long
-//
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    fun insertMeeting(meeting: Meeting): Long
-//
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    fun insertRace(race: Race): Long
-//
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    fun insertRunner(runner: Runner): Long
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertRaceDay(raceDay: RaceDay): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMeeting(meeting: Meeting): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertRace(race: Race): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertRunner(runner: Runner): Long
 
     // Updates.
-//    @Update(onConflict = OnConflictStrategy.IGNORE)
-//    fun updateRaceDay(raceDay: RaceDay): Int
-//
-//    @Update(onConflict = OnConflictStrategy.IGNORE)
-//    fun updateMeeting(meeting: Meeting): Int
-//
-//    @Update(onConflict = OnConflictStrategy.IGNORE)
-//    fun updateRace(race: Race): Int
-//
-//    @Update(onConflict = OnConflictStrategy.IGNORE)
-//    fun updateRunner(runner: Runner): Int
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    fun updateRaceDay(raceDay: RaceDay): Int
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    fun updateMeeting(meeting: Meeting): Int
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    fun updateRace(race: Race): Int
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    fun updateRunner(runner: Runner): Int
 
     // Selects.
-
+    // TBA . . .
 
     // Deletes TBA
-//    @Delete
-//    fun deleteRaceDay(raceDay: RaceDay)
+    @Delete
+    fun deleteRaceDay(raceDay: RaceDay)
+
+    @Delete
+    fun deleteMeeting(meeting: Meeting)
+
+    @Delete
+    fun deleteRace(race: Race)
+
+    @Delete
+    fun deleteRunner(runner: Runner)
+
 }
