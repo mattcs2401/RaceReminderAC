1b8e4435dc17de51f6c193b94fe3dbd99b9549be
mattcs2401 <mattcs2401@gmail.com>
2019-09-12 15:00:05 +1000

    Code changes.

---
+++ app/src/main/java/mcssoft/com/racereminderac/adapter/RaceAdapter2.kt
@@ -0,0 +1,20 @@
+package mcssoft.com.racereminderac.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import mcssoft.com.racereminderac.model.RaceViewModel
+
+class RaceAdapter2(rvModel: RaceViewModel) : RecyclerView.Adapter<RaceViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RaceViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        return 0 //rvModel.
+    }
+
+    override fun onBindViewHolder(holder: RaceViewHolder, position: Int) {
+        // TBA
+    }
+}
---
+++ app/src/main/java/mcssoft/com/racereminderac/model/RaceViewModel2.kt
@@ -0,0 +1,9 @@
+package mcssoft.com.racereminderac.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class RaceViewModel2(application: Application) : AndroidViewModel(application) {
+
+
+}
