1194f831325a05b4018327b93ca4172d29eeda18
mattcs2401 <mattcs2401@gmail.com>
2019-09-13 14:47:41 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/racereminderac/model/RaceViewModel2.kt
+++
@@ -1,9 +0,0 @@
-package mcssoft.com.racereminderac.model
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class RaceViewModel2(application: Application) : AndroidViewModel(application) {
-
-
-}
